1175db820f6f6284b068e4802756bcf1e084d6bc
Samiasecrets <samiasecrets@gmail.com>
2023-08-19 12:55:37 -0700

    main.c

--- _atoi.c
+++ _atoi.c
@@ -8,8 +8,9 @@
  */
 int interactive(info_t *info)
 {
-return (isatty(SIDIN_FILENO) && info->readfd <= 2);
+return (isatty(STDIN_FILENO) && info->readfd <= 2);
 }
+
 /**
  * is_delim - checks if character is a delimeter
  * @c: the char to check
@@ -25,10 +26,11 @@ return (0);
 }
 
 /**
- * _isalpha - checks for alphabetic character
- * @c: The character to input
- * Return: 1 if c is alphabetic, 0 otherwise
+ *_isalpha - checks for alphabetic character
+ *@c: The character to input
+ *Return: 1 if c is alphabetic, 0 otherwise
  */
+
 int _isalpha(int c)
 {
 if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
@@ -38,19 +40,21 @@ return (0);
 }
 
 /**
- * _atoi - converts a string to an integer
- * @s: the string to be converted
- * Return: 0 if no numbers in string, converted number otherwise
+ *_atoi - converts a string to an integer
+ *@s: the string to be converted
+ *Return: 0 if no numbers in string, converted number otherwise
  */
 
 int _atoi(char *s)
 {
 int i, sign = 1, flag = 0, output;
 unsigned int result = 0;
-for (i = 0; s[i] != '\0' && flag != 2; i++)
+
+for (i = 0;  s[i] != '\0' && flag != 2; i++)
 {
 if (s[i] == '-')
 sign *= -1;
+
 if (s[i] >= '0' && s[i] <= '9')
 {
 flag = 1;
@@ -58,12 +62,14 @@ result *= 10;
 result += (s[i] - '0');
 }
 else if (flag == 1)
-	flag = 2;
+flag = 2;
 }
 
 if (sign == -1)
 output = -result;
 else
 output = result;
+
 return (output);
 }
+
